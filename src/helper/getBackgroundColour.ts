977fccbf78167e3fbbba0eca9ff78a7dfd0c72c8
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-08-18 22:33:01 +0700

    set dynamic color in stats component

--- src/helper/getBackgroundColour.ts
+++ src/helper/getBackgroundColour.ts
@@ -20,7 +20,7 @@ export const getBackgroundColour = (type: string) => {
       return "bg-[#D94256] p-4 mt-6 rounded-md flex justify-around items-center max-[1200px]:gap-2 max-[770px]:flex-col-reverse max-[770px]:gap-0 max-[770px]:p-10 max-[450px]:p-4";
     case "flying":
       return "bg-[#9BB4E8] p-4 mt-6 rounded-md flex justify-around items-center max-[1200px]:gap-2 max-[770px]:flex-col-reverse max-[770px]:gap-0 max-[770px]:p-10 max-[450px]:p-4";
-    case "gosht":
+    case "ghost":
       return "bg-[#7975D4] p-4 mt-6 rounded-md flex justify-around items-center max-[1200px]:gap-2 max-[770px]:flex-col-reverse max-[770px]:gap-0 max-[770px]:p-10 max-[450px]:p-4";
     case "ground":
       return "bg-[#D78555] p-4 mt-6 rounded-md flex justify-around items-center max-[1200px]:gap-2 max-[770px]:flex-col-reverse max-[770px]:gap-0 max-[770px]:p-10 max-[450px]:p-4";
@@ -36,52 +36,46 @@ export const getBackgroundColour = (type: string) => {
       return "bg-[#CEC18C] p-4 mt-6 rounded-md flex justify-around items-center max-[1200px]:gap-2 max-[770px]:flex-col-reverse max-[770px]:gap-0 max-[770px]:p-10 max-[450px]:p-4";
     case "steel":
       return "bg-[#B8B8D0] p-4 mt-6 rounded-md flex justify-around items-center max-[1200px]:gap-2 max-[770px]:flex-col-reverse max-[770px]:gap-0 max-[770px]:p-10 max-[450px]:p-4";
-
-    default:
-      break;
   }
 };
 
 export const getStatProgressColour = (type: string) => {
   switch (type) {
     case "water":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#60A5FA] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#60A5FA] ";
     case "fire":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#FC6C6D] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#FC6C6D] ";
     case "grass":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#5DBE62] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#5DBE62] ";
     case "bug":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#9DC130] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#9DC130] ";
     case "dark":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#5F606D] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#5F606D] ";
     case "dragon":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#0773C7] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#0773C7] ";
     case "electric":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#EDD53F] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#EDD53F] ";
     case "fairy":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#EF97E6] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#EF97E6] ";
     case "fighting":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#D94256] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#D94256] ";
     case "flying":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#9BB4E8] ";
-    case "gosht":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#7975D4] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#9BB4E8] ";
+    case "ghost":
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#7975D4] ";
     case "ground":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#D78555] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#D78555] ";
     case "ice":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#98D8D8] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#98D8D8] ";
     case "normal":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#9A9DA1] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#9A9DA1] ";
     case "poison":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#B563CE] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#B563CE] ";
     case "psychic":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#F85888] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#F85888] ";
     case "rock":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#CEC18C] ";
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#CEC18C] ";
     case "steel":
-      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-slate-500 [&::-webkit-progress-value]:bg-[#B8B8D0] ";
-
-    default:
-      break;
+      return "w-full rounded-xl h-2 [&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-md [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-[#B8B8D0] ";
   }
 };
