a1a3a666560116a6ae94ee4227d575ff214c7a79
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-09-18 22:16:16 +0700

    update readme && route

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,20 +1,6 @@
 import Home from "../pages/home/Home";
 import DetailPage from "../pages/detail-page/DetailPage";
 import SearchPage from "../pages/search-page/SearchPage";
-import MyPokemon from "../pages/my-pokemon/MyPokemon";
-import RegionPage from "../pages/region-page/RegionPage";
-// import Loading from "../components/loading/Loading";
-// import EvolutionLine from "../components/detail-page-components/SpritesPokemon";
-
-// const MyPokemon = lazy(() => import("./pages/my-pokemon/MyPokemon"));
-
-// const RegionPageComponent = lazy(
-//   () => import("./pages/region-page/RegionPage")
-// );
-
-// const EvolutionLine = lazy(
-//   () => import("../pages/evolution-line/EvolutionLine")
-// );
 
 export const routes = [
   {
@@ -22,37 +8,9 @@ export const routes = [
     element: <Home />,
   },
   {
-    path: "pokemon/:region",
-    element: <RegionPage />,
-  },
-  // {
-  //   path: "pokemon/:region",
-  //   element: (
-  //     <Suspense fallback={<Loading />}>
-  //       <RegionPageComponent />
-  //     </Suspense>
-  //   ),
-  // },
-  {
     path: "pokemon/detail/:idPokemon",
     element: <DetailPage />,
   },
-  // {
-  //   path: "pokemon/evolutions",
-  //   element: <EvolutionLine />,
-  // },
-  {
-    path: "/my-pokemon",
-    element: <MyPokemon />,
-  },
-  // {
-  //   path: "/my-pokemon",
-  //   element: (
-  //     <Suspense fallback={<Loading />}>
-  //       <MyPokemon />
-  //     </Suspense>
-  //   ),
-  // },
   {
     path: "filter/",
     element: <SearchPage />,
