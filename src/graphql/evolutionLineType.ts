91805d40d7908211b7cff317066fa7c7e187637a
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-09-22 20:44:49 +0700

    add evolution page

--- src/graphql/evolutionLineType.ts
+++ src/graphql/evolutionLineType.ts
@@ -16,4 +16,30 @@ export interface EvolutionLineType {
 
 export interface PokemonEvolutionInterface {
   pokemon_v2_pokemonspecies: EvolutionLineType[];
+  __typename: string;
+}
+
+export interface PokemonType {
+  __typename: string;
+  id: number;
+  name: string;
+  evolves_from_species_id: number | null;
+  pokemon_v2_generation: {
+    __typename: string;
+    id: number;
+  };
+  pokemon_v2_pokemons: Pokemon[];
+  pokemon_v2_pokemonevolutions: {
+    min_level: number;
+    pokemon_v2_evolutiontrigger: {
+      name: string;
+    };
+    pokemon_v2_item: null | string;
+  }[]; // Anda dapat mengganti ini sesuai dengan tipe data sebenarnya
+}
+
+export interface Pokemon {
+  __typename: string;
+  name: string;
+  pokemon_v2_pokemontypes: PokemonType[];
 }
--- src/pages/evolution-line/EvolutionLine.tsx
+++ src/pages/evolution-line/EvolutionLine.tsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Loading from "../../components/loading/Loading";
 import { useLazyQuery } from "@apollo/client";
 import useFetchMore from "../../helper/useFetchMore";
-import { PokemonEvolutionInterface } from "../../graphql/evolutionLineType";
+import {
+  PokemonEvolutionInterface,
+  EvolutionLineType,
+} from "../../graphql/evolutionLineType";
 import { GET_POKEMON_EVOLUTION } from "../../graphql/query/getPokemonEvolution";
 import { getPokemonImage } from "../../api/getDataPokemon";
 import { upperFirstCharacter } from "../../helper/upperFirstCharacter";
 
 const EvolutionLine = () => {
   const [evolutionData, setEvolutionData] = useState<
-    PokemonEvolutionInterface[]
+    PokemonEvolutionInterface[] | object[]
   >([]);
+
   const [signal, setSignal] = useState(0);
+  const navigate = useNavigate();
   const { errorFetching, nextDataEvolutionLine } = useFetchMore(
     evolutionData.length
   );
@@ -45,6 +51,8 @@ const EvolutionLine = () => {
     setSignal(signal + 1);
   };
 
+  // console.log(evolutionData);
+
   return (
     <div className="w-full">
       <h1 className="p-2">Evolution line page</h1>
@@ -54,29 +62,47 @@ const EvolutionLine = () => {
         hasMore={true}
         loader={<></>}
       >
-        {evolutionData.map((pokemonLine, index) => (
-          <div
-            key={index}
-            className="relative grid grid-flow-row gap-4 text-neutral-600 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 p-2 overflow-hidden'"
-          >
-            {pokemonLine.pokemon_v2_pokemonspecies.map((pokemon) => (
-              <div
-                key={pokemon.name}
-                title={pokemon.name}
-                className="p-4 flex items-center bg-slate-300 bg-dark-card group rounded-xl "
-              >
-                <img
-                  className="w-[180px]"
-                  src={getPokemonImage(pokemon.id)}
-                  alt={pokemon.name}
-                />
-                <h2 className="font-bold text-xl">
-                  {upperFirstCharacter(pokemon.name)}
-                </h2>
-              </div>
-            ))}
-          </div>
-        ))}
+        {evolutionData.map((pokemonLine: any, index: number) => {
+          // console.log(pokemonLine);
+
+          return (
+            <div
+              key={index}
+              className="relative grid grid-flow-row gap-4 text-neutral-600 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 p-2 overflow-hidden'"
+            >
+              {pokemonLine.pokemon_v2_pokemonspecies.map(
+                (pokemon: EvolutionLineType) => {
+                  return (
+                    <div
+                      key={pokemon.name}
+                      title={pokemon.name}
+                      className="p-4 flex items-center bg-slate-300 bg-dark-card group rounded-xl "
+                    >
+                      <img
+                        className="w-[180px]"
+                        src={getPokemonImage(pokemon.id)}
+                        alt={pokemon.name}
+                      />
+                      <div>
+                        <h2 className="font-bold text-xl">
+                          {upperFirstCharacter(pokemon.name)}
+                        </h2>
+                        <p
+                          onClick={() =>
+                            navigate(`/pokemon/detail/${pokemon.id}`)
+                          }
+                          className="cursor-pointer hover:underline"
+                        >
+                          Show Details
+                        </p>
+                      </div>
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          );
+        })}
       </InfiniteScroll>
       <div className="w-full flex justify-center items-center mt-24">
         <span className="loading loading-spinner loading-lg text-primary -mt-28"></span>
